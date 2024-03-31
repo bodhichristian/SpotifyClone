e51adf84cbc68e4458667c9a72ab9cdb8412feb2
bodhichristian <clavelle23@gmail.com>
2024-03-31 19:24:19 -0400

    Minor udpates

--- SpotifyClone/Components/PlaylistDescriptionCell.swift
+++ SpotifyClone/Components/PlaylistDescriptionCell.swift
@@ -26,6 +26,61 @@ struct PlaylistDescriptionCell: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
 
             madeForYouSection
+            
+            Text(subheadline)
+            
+            HStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    Image(systemName: "plus.circle")
+                        .padding(8)
+                        .background(Color.spotifyBlack.opacity(0.0001))
+                        .onTapGesture {
+                            onAddToPlaylistPressed?()
+                        }
+                    
+                    Image(systemName: "arrow.down.circle")
+                        .padding(8)
+                        .background(Color.spotifyBlack.opacity(0.0001))
+                        .onTapGesture {
+                            onAddToPlaylistPressed?()
+                        }
+                    
+                    Image(systemName: "square.and.arrow.up")
+                        .padding(8)
+                        .background(Color.spotifyBlack.opacity(0.0001))
+                        .onTapGesture {
+                            onAddToPlaylistPressed?()
+                        }
+                    
+                    Image(systemName: "ellipsis")
+                        .padding(8)
+                        .background(Color.spotifyBlack.opacity(0.0001))
+                        .onTapGesture {
+                            onAddToPlaylistPressed?()
+                        }
+                }
+                .offset(x: -8)
+                
+                HStack(spacing: 8) {
+                    Image(systemName: "shuffle")
+                        .padding(8)
+                        .background(Color.spotifyBlack.opacity(0.0001))
+                        .font(.system(size: 24))
+                        .onTapGesture {
+                            onAddToPlaylistPressed?()
+                        }
+                    
+                    Image(systemName: "play.circle.fill")
+                        .padding(8)
+                        .font(.system(size: 46))
+                        .background(Color.spotifyBlack.opacity(0.0001))
+                        .onTapGesture {
+                            onAddToPlaylistPressed?()
+                        }
+                }
+                .foregroundStyle(.spotifyGreen)
+            }
+            .font(.title2)
         }
         .font(.callout)
         .fontWeight(.medium)
